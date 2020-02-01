40100de39c82789c05c0473a08c5900e5bda8aa2
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-01 19:20:33 +0100

    updated

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,18 +6,27 @@
 
 
 <div class="container">
+    <span>LOGIN</span>
     <form>
    
         <div class="first">
         <div id="form"> 
-        <div>
-            <label for='username'>USERNAME</label>
-        <input type="text" name="username" placeholder="Enter Username" required />
+
+        <div id="user">
+        <label for='username'>USERNAME</label>
+        <i class="fa fa-user"><input type="text" name="username"  placeholder="Enter Username" required /></i>
         </div>
+
         <div>
             <label for="password">PASSWORD</label>
         <input type="password" name="password" placeholder="Enter Password" required />
         </div>
+
+        <div>
+            <input type="checkbox" name="remember" > REMEMBER ME
+        </div>
+
+
         </div>
         </div>
     </form>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,6 +9,10 @@
   <meta name="author" content="Adejobi Ezekiel">
   <link rel="shortcut icon" href="https://res.cloudinary.com/ezeko/image/upload/v1566502038/images/31517324_l4rbyl.jpg" 
         type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <!--Font awesome 5 link-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        crossorigin="anonymous">
 
   <link rel="stylesheet" href="{{URL::asset('css/styles.css') }}" >
 
