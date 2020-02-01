17ce7d7b212005acf42a64a05aa0e52bb451cb54
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-01 10:34:23 +0100

    add registration frontend beginning

---
+++ resources/views/register.blade.php
@@ -0,0 +1,14 @@
+@extends('template')
+
+@section('title', 'E-Folio :: Register Account')
+
+@section('content')
+
+<form>
+<div class="first"> 
+<input type="text" placeholder="Enter FullName" required />
+</div>
+</form>
+
+
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>@yield('title')</title>
+  <meta name="description" content="Adejobi Ezekiel portfolio">
+  <meta name="author" content="Adejobi Ezekiel">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+@yield('content')
+  <script src="js/scripts.js"></script>
+</body>
+</html>
