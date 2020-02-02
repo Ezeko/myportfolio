7802a7a2a79e6c4c664495bfaa31c86ab26d2162
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-02 15:17:10 +0100

    style:
    style registration page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,12 @@
 @extends('template')
 
-@section('title', 'E-Folio :: Sign in')
+@section('title', 'E-Folio :: Register Account')
 
 @section('content')
 
-
+<link rel="stylesheet" href="{{URL::asset('css/styles2.css') }}" >
 <div class="container">
-    <span>LOGIN</span>
+    <span>REGISTER</span>
     <form>
    
         <div class="first">
@@ -17,13 +17,18 @@
         <input type="text" class="user" name="username"  placeholder=" Enter Username" required />
         </div>
 
+        <div id="user">
+        <label for='username'>EMAIL</label>
+        <input type="text" class="user" name="email"  placeholder=" Enter Email" required />
+        </div>
+
         <div>
             <label for="password">PASSWORD</label>
         <input type="password" name="password" placeholder="Enter Password" required />
         </div>
 
         <div>
-            <input type="checkbox" name="remember" > REMEMBER ME
+            <input type="checkbox" name="remember"  checked> AGREE TO TERMS AND CONDITIONS
         </div>
 
         <button type="submit" id="sub"><div ><i class="fa fa-arrow-right"></i></div></button>
@@ -33,9 +38,9 @@
         </div>
     </form>
         <div class="second">
-            <p>Don't Have an account?? </p><br>
+            <p>Already Have an account?? </p><br>
             <div id="signup">
-            <a  href="/register"> SIGN UP </a>
+            <a  href="/"> SIGN IN </a>
             </div>
         </div>
 </div>
