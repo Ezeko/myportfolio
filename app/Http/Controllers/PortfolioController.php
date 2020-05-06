e000221920bf8fd8b2c983112e54eeb9e1d43536
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-05-06 15:36:24 +0100

    feat
    make upload function for adding portfolios

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -13,4 +13,102 @@ class PortfolioController extends Controller
 
         echo $username."'s portfolio";
     }
+
+    public function addPortfolio (Request $request)
+    {
+        // adding portfolio method
+        request()->validate([
+
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+
+        ]);
+
+  
+
+        $imageName = time().'.'.request()->image->getClientOriginalExtension();
+
+  
+
+        request()->image->move(public_path('images'), $imageName);
+
+  
+
+        return back()
+
+            ->with('success','You have successfully upload image.')
+
+            ->with('image',$imageName);
+
+        if ( $request->image2 )
+        {
+            request()->validate([
+
+                'image2' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    
+            ]);
+            
+            $imageName = time().'.'.request()->image2->getClientOriginalExtension();
+
+    
+
+            request()->image2->move(public_path('images'), $imageName);
+
+    
+
+            return back()
+
+                ->with('success','You have successfully upload image.')
+
+                ->with('image',$imageName);
+        }
+
+        
+        if ( $request->image3 )
+        {
+            request()->validate([
+
+                'image3' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    
+            ]);
+            
+            $imageName = time().'.'.request()->image3->getClientOriginalExtension();
+
+    
+
+            request()->image3->move(public_path('images'), $imageName);
+
+    
+
+            return back()
+
+                ->with('success','You have successfully upload image.')
+
+                ->with('image',$imageName);
+        }
+
+        
+        if ( $request->image4 )
+        {
+            request()->validate([
+
+                'image4' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    
+            ]);
+            
+            $imageName = time().'.'.request()->image4->getClientOriginalExtension();
+
+    
+
+            request()->image4->move(public_path('images'), $imageName);
+
+    
+
+            return back()
+
+                ->with('success','You have successfully upload image.')
+
+                ->with('image',$imageName);
+        }
+        
+    }
 }
